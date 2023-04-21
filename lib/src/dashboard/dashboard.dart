44fdcb7cc28274112a8ed8be825e27cd51d2e5d3
LynnKhantKyaw <lynnkhant.ky@gmail.com>
2023-04-21 21:10:12 +0630

    .|.

--- lib/src/dashboard/dashboard.dart
+++ lib/src/dashboard/dashboard.dart
@@ -1,34 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:hero_to_zero/db/db_names.dart';
-import 'package:hero_to_zero/routes/route_names.dart';
-import 'package:hero_to_zero/routes/routes.dart';
 import 'package:hero_to_zero/src/auth/model/register/register_model.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:hero_to_zero/src/auth/service/store_register_service.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  late Stream<List<RegisterModel>> streamUser;
+
+  ObjectBox? _objectBox;
+
+  @override
+  void initState() {
+    if (_objectBox == null) {}
+    streamUser = _objectBox!.getUsers();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            InkWell(
-                onTap: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  for (var i in prefs.getKeys()) {
-                    prefs.remove(i);
-                  }
-                  GoRouter.of(navigatorKey.currentContext!)
-                      .pushNamed(RouteNames.initalPage);
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return const Text('data');
                 },
-                child: const Text('dfd')),
-          ],
-        ),
+                separatorBuilder: (_, __) => const SizedBox(height: 10),
+                itemCount: 1),
+          ),
+        ],
       ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        var data = RegisterModel(
+            id: 0,
+            username: 'username',
+            email: 'email',
+            password: 'password',
+            confirmpassword: 'confirmpassword');
+        _objectBox!.insertUser(data);
+      }),
     );
   }
 }
